62660a6e3b2c8dc85477394e13a799617cd212c6
L3tterFake <104180634+L3tterFake@users.noreply.github.com>
2025-12-02 19:35:20 +0700

    commit

--- Scripts/main.js
+++ Scripts/main.js
@@ -114,20 +114,16 @@ particlesJS("bg", {
   
   /* ---- stats.js config ---- */
   
-  var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector('.js-count-particles');
-  update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
+  import Grid1Background from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.16/build/backgrounds/grid1.cdn.min.js'
+
+const bg = Grid1Background(document.getElementById('webgl-canvas'))
+
+const button1 = document.getElementById('colors-btn')
+button1.addEventListener('click', () => {
+  bg.grid.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
+  bg.grid.light1.color.set(0xffffff * Math.random())
+  bg.grid.light1.intensity = 500 + Math.random() * 1000
+  bg.grid.light2.color.set(0xffffff * Math.random())
+  bg.grid.light2.intensity = 250 + Math.random() * 250
+})
+
